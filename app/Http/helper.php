786c6442fec5d8912db72c9bb42c8f561d73918c
codexindia <sudiptobain8@gmail.com>
2024-06-10 18:08:17 +0530

    first commit

---
+++ app/Http/helper.php
@@ -0,0 +1,78 @@
+<?php
+
+use App\Models\Settings;
+use App\Models\CoinsTransaction;
+use App\Models\User;
+use Berkayk\OneSignal\OneSignalFacade as OneSignal;
+use Illuminate\Support\Facades\DB;
+
+if (!function_exists('get_setting')) {
+    function get_setting($key, $group = 'general')
+    {
+        $result = Settings::where('name', $key)->where('group', $group)->first('payload');
+        if ($result != null) {
+            return str_replace('"', '', $result->payload);;
+        } else {
+            return 0;
+        }
+    }
+}
+if (!function_exists('coin_action')) {
+    function coin_action(int $user_id, float $coins, $type = "debit", $description = null, $meta = [])
+    {
+
+        $user = User::findOrFail($user_id);
+        if ($user)
+            $transaction = new CoinsTransaction;
+        $transaction->user_id = $user_id;
+        $transaction->coin = $coins;
+        $transaction->transaction_type = $type;
+        $transaction->description = $description;
+        $transaction->transaction_id = 'MST' . $user_id . time() . rand('10', '99');
+        $transaction->status = 'success';
+        $transaction->meta = json_encode($meta);
+        if ($transaction->save()) {
+            if ($type == "credit") {
+                if ($user->increment('coin', $coins)) {
+
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                if ($user->decrement('coin', $coins)) {
+
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+    function sendpush($user_id, $text,$heading = null,$params = [])
+    {
+       
+        $params['android_channel_id'] = '7fbda4a1-81c5-4eb6-9936-a80543c5c06f';
+       
+        if ($user_id == null) {
+            OneSignal::addParams($params)->sendNotificationToAll(
+                $text,
+                $url = null,
+                $data = null,
+                $buttons = null,
+                $schedule = null,
+                $heading
+            );
+        } else {
+            OneSignal::addParams($params)->sendNotificationToExternalUser(
+                $text,
+                $user_id,
+                $url = null,
+                $data = null,
+                $buttons = null,
+                $schedule = null
+            );
+        }
+    }
+}
---
+++ app/Models/User.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Schema\Builder;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Storage;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'username',
+        'date_of_birth',
+        'phone_number',
+        'country_code',
+        'profile_pic',
+        'language',
+        'refer_code',
+        'referred_by',
+        'coin'
+    ];
+    public function getProfilePicAttribute($value)
+    {
+        if(!$value == null)
+        {
+            return asset(Storage::url($value));
+        }
+       return url('images/pp.jpg');
+    }
+    function toCamelCaseMethod1($inputString)
+    {
+        $inputString = strtolower($inputString);
+        $words = explode(' ', $inputString);
+        $camelCase = "";
+
+        for ($i = 0; $i < count($words); $i++) {
+            $camelCase .= ucfirst($words[$i]) . ' ';
+        }
+
+        return $camelCase;
+    }
+    
+
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+
+    ];
+    public function Country(): HasOne
+    {
+        return $this->hasOne(CountryInfo::class, 'dial_code', 'country_code');
+    }
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => $this->toCamelCaseMethod1($value),
+        );
+    }
+    protected function email(): Attribute
+    {
+       
+        return Attribute::make(
+            get: fn ($value) => strtolower($value),
+        );
+    }
+   
+    public function GetMining()
+    {
+       return $this->hasMany(MiningSession::class,'user_id','id');
+    }
+}
